bc0f1a971834c66c0fd61c6eb9f2d6db9f47e2f8
Ubuntu <ubuntu@ip-172-31-31-194.us-west-2.compute.internal>
2020-05-03 15:03:49 +0000

    Simplify flask app, it's not important the flask app complexity

--- app/app.py
+++ app/app.py
@@ -1,76 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask.logging import create_logger
 import logging
-
-import pandas as pd
-from sklearn.externals import joblib
-from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
-def scale(payload):
-    """Scales Payload"""
-    LOG.info(f"Scaling Payload: \n{payload} ")
-    scaler = StandardScaler().fit(payload.astype(float))
-    scaled_adhoc_predict = scaler.transform(payload.astype(float))
-    return scaled_adhoc_predict
+@app.route("/ping", methods=['GET'])
+def ping():
+    return jsonify(
+            status="ok"
+        )
 
-@app.route("/", methods=['GET'])
-def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
-    return html.format(format)
-
-@app.route("/mini-game/<name>", methods=['POST'])
-@app.route("/mini-game", methods=['GET'])
-def mini_game():
-    return flask.render_template("mini-game.html",name=name)
-
-@app.route("/predict", methods=['POST'])
-def predict():
-    """Performs an sklearn prediction
-
-        input looks like:
-        {
-        "CHAS":{
-        "0":0
-        },
-        "RM":{
-        "0":6.575
-        },
-        "TAX":{
-        "0":296.0
-        },
-        "PTRATIO":{
-        "0":15.3
-        },
-        "B":{
-        "0":396.9
-        },
-        "LSTAT":{
-        "0":4.98
-        }
-
-        result looks like:
-        { "prediction": [ <val> ] }
-
-        """
-
-    # Logging the input payload
-    json_payload = request.json
-    LOG.info(f"JSON payload: \n{json_payload}\n\n")
-    inference_payload = pd.DataFrame(json_payload)
-    LOG.info(f"Inference1 payload DataFrame: \n{inference_payload}\n\n")
-    # scale the input
-    scaled_payload = scale(inference_payload)
-    # get an output prediction from the pretrained model, clf
-    prediction = list(clf.predict(scaled_payload))
-    # TO DO:  Log the output prediction value
-    LOG.info(f"Data prediction: \n{prediction}\n\n")
-    return jsonify({'prediction': prediction})
+@app.route("/mini-game/<name>", methods=['GET'])
+def minigame(name):
+    LOG.info(f"Get Minigame")
+    return render_template("mini-game.html", name=name)
 
 if __name__ == "__main__":
     # load pretrained model as clf
-    clf = joblib.load("./model_data/boston_housing_prediction.joblib")
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
